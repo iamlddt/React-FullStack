f9ba645c74dd92b2917de702f657fc7e33583d40
Trung <ledinhduytrungcons@gmail.com>
2024-04-16 22:59:28 +0700

    SyntheticEvent

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,10 +9,16 @@ class MyComponent extends React.Component {
         age: '32'
     }
 
+    handleOnclick = (event) => {
+        console.log('>> Click my button')
+        console.log('My name is ', this.state.name)
+    };
+
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={(event) => { this.handleOnclick(event) }}>Click me</button>
             </div>
         )
     }
