fa2b8a2b2ec5c5df78fa13c3f2451c30fa196c20
Trung <ledinhduytrungcons@gmail.com>
2024-04-17 14:43:49 +0700

    CSS

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Displayinfor.scss';
 
 class DisplayInfor extends React.Component {
 
@@ -17,7 +18,7 @@ class DisplayInfor extends React.Component {
         console.log(listUsers)
 
         return (
-            <div>
+            <div className="display-infor-container">
 
                 <div>
                     <button
